20adfa0879278bacef8ab36239df600bdb40f160
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-19 13:01:38 -0700

    added project details to student info page

--- hackbright-web.py
+++ hackbright-web.py
@@ -15,16 +15,19 @@ def get_student():
 
     github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
-    html = render_template('student_info.html', first=first, last=last, github=github)
+    projects = hackbright.get_grades_by_github(github)
+    html = render_template('student_info.html', first=first, last=last, github=github, projects=projects)
     return html
 
 @app.route('/form')
 def form_route():
+    """Route to pass data from /student to /student-add """
     return render_template('add_student.html')
 
 
 @app.route("/student-add", methods=["POST"])
 def student_add():
+    """Create new student and update the student database """
     first = request.form.get("firstname")
     last = request.form.get("lastname")
     github = request.form.get("github")
@@ -33,9 +36,17 @@ def student_add():
 
     return render_template('acknowledge.html', first=first, last=last, github=github)
 
-    
+# @app.route('/project-form', methods=['GET'])
+# def project_route():
+#     """Route to form that collects project title info """
+#     return render_template('project.html')
+
+@app.route('/project')
+def project_info():
+    title, description, max_grade = hackbright.get_project_by_title(project)
 
 
+    return render_template('project.html',title=title, description=description, max_grade=max_grade)
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
